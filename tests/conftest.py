12293041e17622d4c26728c0b82a0eb456fc1698
миша <misatoropkov@gmail.com>
2024-12-03 21:06:29 +0300

    03.12 already pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 # conf for test_processing
+
 not_sorted_by_date_1 = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
