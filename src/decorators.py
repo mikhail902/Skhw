7c3ef055b5bc48a4c3f5577621ee98c7507f698f
миша <misatoropkov@gmail.com>
2024-12-12 19:05:41 +0300

    12.12 hw

--- src/decorators.py
+++ src/decorators.py
@@ -1,34 +1,48 @@
-import os
-
-PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "data", "log.scripts.txt")
+import time
 
 
-def log(filename=None):
-    def my_decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+def log(filename=None) -> any:
+    def wrapper(function):
+        def inner(*args, **kwargs):
             try:
-                result = func(*args, **kwargs)
+                time_1 = time.time()
+                result = function(*args, **kwargs)
+                time_2 = time.time()
+                time_of_work = time_2 - time_1
                 if filename is not None:
-                    with open(PATH_TO_FILE, "a", encoding="utf-8") as file:
-                        return file.write(f'Выполнена функция {func.__name__}, результат {result}.\n')
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"""\nФункция успешно выполнена. Результат функции: {result}""")
+                        file.write(f"""\nВремя работы: {time_of_work} секунд""")
+                        return result
                 else:
+                    print(result)
                     return result
-            except Exception as error:
-                if filename is not None:
-                    with open(PATH_TO_FILE, "a", encoding="utf-8") as file:
-                        return file.write("При выполнении функции произошла ошибка")
-                else:
-                    return "Функция выполнена с ошибкой"
+            except Exception as e:
+                time_1 = time.time()
+                result = function(*args, **kwargs)
+                time_2 = time.time()
+                time_of_work = time_2 - time_1
+                if file is not None:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(
+                            f"""\nПри выполнении функции произошла ошибка {e}. Результат функции: {result}\n"""
+                        )
+                return "Ошибка"
 
-        return wrapper
+        return inner
 
-    return my_decorator
-
-
-@log(filename="mylog.txt")
-def my_function():
-    pass
+    return wrapper
 
 
-my_function(10, 20)
+@log(filename="results_of_decorators.txt")
+def log_first(x, y):
+    return x + y
+
+
+@log(filename=None)
+def log_second(x, y):
+    return x + y
+
+
+log_first(1, 2)
+log_second(4, 6)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,10 @@
-import pytest
+from src.decorators import log_first, log_second
 
-def test_
+
+def test_log(capsys):
+    assert log_first(4, 6) == 10
+    assert log_first(0, 0) == 0
+    assert log_first(" ", " ") == "  "
+    log_second(1, 5)
+    captured = capsys.readouterr()
+    assert captured.out == "6\n"
