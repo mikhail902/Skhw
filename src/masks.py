d5b70f6495a497530398165a573cb718553a4627
миша <misatoropkov@gmail.com>
2024-12-23 23:21:27 +0300

    15.12 some fixes

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,13 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Users/Sator/PycharmProjects/PythonProject3/logs/masks_data.log", "a")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card: str) -> str:
     """Функция скрытия карты"""
     mask_card = ""
@@ -10,9 +20,11 @@ def get_mask_card_number(card: str) -> str:
                     card[:i] + card[i : i + 4] + " " + card[i + 4 : i + 6] + 2 * "*" + " " + 4 * "*" + " " + card[-4:]
                 )
             break
+    logger.info("Функция get_mask_card_number выполнена успешно")
     return mask_card
 
 
 def get_mask_account(mask_account_card: str) -> str:
     """Функция номера аккаунта"""
+    logger.info("Функция get_mask_account выполнена успешно")
     return "Счет" + " " + 2 * "*" + mask_account_card[-4:]
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import json
+import logging
 
 from src.external_api import conversion
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("C:/Users/Sator/PycharmProjects/PythonProject3/logs/utils_data.log", "a")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def dict_transactions(url: str) -> list:
@@ -8,9 +16,10 @@ def dict_transactions(url: str) -> list:
     try:
         with open(url, "r", encoding="utf-8") as f:
             data_list = json.load(f)
+            logger.info("Список сконвертировался")
             return data_list
     except (FileNotFoundError, TypeError, json.JSONDecodeError) as e:
-        print(f"Ошибка чтения файла {e}")
+        logger.error(f"Ошибка чтения файла в функции dict_transactions, {e}")
         return []
 
 
@@ -22,9 +31,11 @@ def sum_transactions(transaction: list) -> float:
             tr_in_ruble = conversion(tr)
             float_tr_in_ruble = float(tr_in_ruble)
             sum_ruble += float_tr_in_ruble
+        logging.info("Функция sum_transactions успешно выполнена")
         return sum_ruble
     except ValueError as e:
-        print(f"Ошибка, {e}")
+        logging.error(f"Ошибка в функции sum_transactions, {e}")
+        return 0
 
 
 file_path = "C:/Users/Sator/PycharmProjects/PythonProject3/data/operation.json"
