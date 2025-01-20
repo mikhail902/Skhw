44683ffb51616412c309abc0964d7c30b9c0c5a8
миша <misatoropkov@gmail.com>
2025-01-20 23:07:23 +0300

    20.01 without tests

--- src/filtered_operations.py
+++ src/filtered_operations.py
@@ -1,4 +1,6 @@
 import re
+from src.excel_csv_utils import *
+from collections import Counter
 
 PATH_TO_EXCEL = "C:/Users/Sator/PycharmProjects/PythonProject3/data/excel_file.xlsx"
 PATH_TO_CSV = "C:/Users/Sator/PycharmProjects/PythonProject3/data/csv_file.csv"
@@ -12,3 +14,14 @@ def search_of_operations(operations: list, key: str) -> list:
         if pattern.search(str(dict_number["description"])):
             new_list.append(dict_number)
     return new_list
+
+
+def count_operations_by_category(operations: list, category_key_str: str) -> any:
+    """Функция подсчета операций по категориям"""
+    categories = [op[category_key_str] for op in operations if category_key_str in op]
+    return Counter(categories)
+
+
+category_key = "to"  # задаем категорию
+default_list = csv_to_list_of_dicts(PATH_TO_CSV)
+print(count_operations_by_category(default_list, category_key))
